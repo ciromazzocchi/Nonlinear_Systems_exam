9da8677f6825ca25b47dbd2c560d5515da91894f
ciromazzocchi <ciro.mazzocchi@outlook.com>
2020-08-26 08:34:19 +0200

    Update files

--- plot_pp_PWS.m
+++ plot_pp_PWS.m
@@ -9,14 +9,14 @@ beta = 0.4;
 gamma = 1/14;
 mu = 0.2;
 delta = 0.1;
+R0 = beta/(gamma + mu);
 
-%% Plot
-% Print ROI
+%% Print ROI
 fig = figure();
 hold on
 plot([0 1 0 0 0 1], [0 0 0 1 1 0], 'g');
 
-% Plot vector field
+%% Plot vector field
 [xs,xi] = meshgrid(0:0.05:1.1,-0.1:0.05:1);
     [r,c] = size(xs);
     Sd = zeros(r,c);
@@ -30,21 +30,54 @@ plot([0 1 0 0 0 1], [0 0 0 1 1 0], 'g');
     end
 quiver(xs,xi,Sd,Id);
 
-% Plot equilibrium point
+%% Plot sigma
 
-%% Define initial point
+if R0 < 1
+    hold on
+    plot([0 1],[delta delta], 'r--');
+    hold off
+else
+    hold on
+    plot([0 1/R0],[delta delta], 'r--');
+    hold on
+    plot([1/R0 1],[delta delta], 'r');
+    hold off
+end
+
+%% Plot equilibrium point
+if R0 < 1
+    roots = [1,0];
+elseif 1 > R0 && R0 > 1+delta*beta/mu 
+    roots = [1 0; 1/R0 (mu/beta)*(R0-1)];
+else
+    roots = [1 0; 1-delta*((mu+gamma)/mu) delta];
+end
+hold on
+scatter(roots(:,1),roots(:,2),'b','filled');
+hold off
+
+%% Plot trajectory
+% Define initial point
 x0 = [0.1; 0.1];
 fun = @(x) SIR_PWS(0,x,mu,beta,gamma,delta);
 roots = fsolve(fun,x0);
-scatter(roots(1,:)',roots(2,:)','b','filled');
-
 
 % Plot trajectory
 [~,~,x0] = getInitialPoint();
+[n_row,~] = size(x0);
+for ii = 0:n_row-1
+    if x0(n_row-ii,2) == delta
+        x0(n_row-ii,:) = [];
+    end
+end
 odefun = @(t,x) SIR_PWS(t,x,mu,beta,gamma,delta);
 printTrajectory(fig,x0,odefun);
 hold off
 
-% Figure paramters
-legendLabel = {'Region of interest','Vector field','Equilibrum point'};
+%% Figure paramters
+if R0 < 1
+    legendLabel = {'Region of interest','Vector field','Crossing region','Equilibrum point'};
+else
+    legendLabel = {'Region of interest','Vector field','Crossing region','Sliding region','Equilibrum point'};
+end
 setFigure(fig,"Phase Portrait - R_0 < 1",legendLabel);
--- utility/SIR_PWS.m
+++ utility/SIR_PWS.m
@@ -3,14 +3,16 @@ function [dxdt] = SIR_PWS(t,x,mu,beta,gamma,delta)
     xi = x(2);
     dxdt = zeros(size(x));
     
+    R0 = beta/(gamma+mu);
+    
     if xi < delta
         dxdt(1) = mu - mu*xs - beta*xs*xi;
         dxdt(2) = beta*xs*xi - (gamma + mu)*xi;
     elseif xi > delta
         dxdt(1) = mu - mu*xs;
         dxdt(2) = -(gamma + mu)*xi;
-    else
-        dxdt(1) = mu - mu*xs - beta*xs*xi + mu - mu*xs;
-        dxdt(2) = beta*xs*xi - (gamma + mu)*xi -(gamma + mu)*xi;
+    elseif xi == delta && xs > 1/R0
+        dxdt(1) = mu - mu*xs - (gamma+mu)*delta;
+        dxdt(2) = 0;
     end
 end
