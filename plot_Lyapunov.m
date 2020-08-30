13650f15cfce675e4ab564374186f6d3c89fd274
ciromazzocchi <ciro.mazzocchi@outlook.com>
2020-08-30 22:40:49 +0200

    Now script to plot Lyapunov function are connected

--- plot_Lyapunov.m
+++ plot_Lyapunov.m
@@ -15,14 +15,27 @@ beta = 0.4;
 gamma = 1/14;
 mu = 0.2;
 
+R0 = beta/(gamma+mu);
+
 xs = sym('x','real');
 xi = sym('y','real');
-xse = (gamma+mu)/beta;
-xie = (mu/beta)*(beta/(gamma+mu) - 1);
+
+if R0 < 1
+    xse = 1;
+    xie = 0;
+else
+    xse = (gamma+mu)/beta;
+    xie = (mu/beta)*(beta/(gamma+mu) - 1);
+end
 
 %% Lyapunov function
-V = xs - xse*log(xs/xse) + xi - xie*log(xi/xie);
-Vd = (1- xse/xs)*(mu-mu*xs-beta*xi*xs) + (1-xie/xi).*(beta*xs*xi-xi*(gamma+mu));
+if R0 < 1
+    V = 0.0000001*xs+xi+0.1;
+    Vd = (beta*xs*xi-xi*(gamma+mu));
+else
+    V = xs - xse*log(xs/xse) + xi - xie*log(xi/xie);
+    Vd = (1- xse/xs)*(mu-mu*xs-beta*xi*xs) + (1-xie/xi).*(beta*xs*xi-xi*(gamma+mu));
+end
 
 %% Plot V
 plotLyapunov(V,[xs xi],[xse xie],"V",[0 1 0 1]);
