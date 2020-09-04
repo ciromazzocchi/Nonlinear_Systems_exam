b9ee2003f00263937ed808fbdd9bbeb414411c00
ciromazzocchi <ciro.mazzocchi@outlook.com>
2020-09-04 15:54:54 +0200

    Update

--- SIR_PWS_model_evolution.m
+++ SIR_PWS_model_evolution.m
@@ -6,39 +6,78 @@ addpath('./utility');
 
 %% Define parameter of simulation
 beta = 0.4;
-gamma = 1/14;
 mu = 0.2;
 delta = 0.1;
 
-R0 = beta/(gamma+mu);
+gamma1 = 0.3;
+gamma2 = 0.19;
+gamma3 = 1/14;
 
-%% Define initial point
-S0 = 0.3;
+S0 = 0.70;
 tf = 50;
 
-%% Tracjectory
-odefun = @(t,x) SIR_PWS(t,x,mu,beta,gamma,delta);
+%% R0 < 1
+% Trajectory
+odefun = @(t,x) SIR_PWS(t,x,mu,beta,gamma1,delta);
 [t,x] = ode45(odefun,[0 tf],[S0,1-S0]);
 S = x(:,1);
 I = x(:,2);
 R = 1 - S - I;
 
-%% Plot
-% Print ROI
-fig = figure();
+% Plot
+figure();
 plot(t,S,'y'); hold on;
 plot(t,I,'r'); hold on;
 plot(t,R,'g'); hold on;
 
-% Figure paramters
-if R0 < 1
-    title("Time evolution - R_0 < 1");
-else
-    title("Phase Portrait - R_0 > 1");
-end
-
+% Figure properties
+title("Time evolution - R_0 < 1");
 legend({'Susceptible','Infected','Removed'});
 xlabel('t [day]');
+ylabel('S,I,R [%]');
+ylim([0 1]);
+
+%% 1+δ*(β/μ) > R0 > 1 
+% Trajectory
+odefun = @(t,x) SIR_PWS(t,x,mu,beta,gamma2,delta);
+[t,x] = ode45(odefun,[0 tf],[S0,1-S0]);
+S = x(:,1);
+I = x(:,2);
+R = 1 - S - I;
+
+% Plot
+figure();
+plot(t,S,'y'); hold on;
+plot(t,I,'r'); hold on;
+plot(t,R,'g'); hold on;
+
+% Figure properties
+title("Phase Portrait - 1+δ*(β/μ) > R_0 > 1");
+legend({'Susceptible','Infected','Removed'});
+xlabel('t [day]');
+ylabel('S,I,R [%]');
+ylim([0 1]);
+
+%% R0 > 1+δ*(β/μ)
+% Trajectory
+odefun = @(t,x) SIR_PWS(t,x,mu,beta,gamma3,delta);
+[t,x] = ode45(odefun,[0 tf],[S0,1-S0]);
+S = x(:,1);
+I = x(:,2);
+R = 1 - S - I;
+
+% Plot
+figure();
+plot(t,S,'y'); hold on;
+plot(t,I,'r'); hold on;
+plot(t,R,'g'); hold on;
+
+% Figure properties
+title("Phase Portrait - R_0 > 1+δ*(β/μ)");
+legend({'Susceptible','Infected','Removed'});
+xlabel('t [day]');
+ylabel('S,I,R [%]');
+ylim([0 1]);
 
 %% Clean
 rmpath('./utility');
--- SIR_model_evolution.m
+++ SIR_model_evolution.m
@@ -6,37 +6,55 @@ addpath('./utility');
 
 %% Define parameter of simulation
 beta = 0.4;
-gamma = 0.25;
+gamma1 = 0.3;
+gamma2 = 1/14;
 mu = 0.2;
-R0 = beta/(gamma+mu);
 
-%% Define initial point
-S0 = 0.3;
+S0 = 0.7;
 tf = 50;
 
-%% Tracjectory
-odefun = @(t,x) SIR(t,x,mu,beta,gamma);
+%% R0 < 1
+% Tracjectory
+odefun = @(t,x) SIR(t,x,mu,beta,gamma1);
 [t,x] = ode45(odefun,[0 tf],[S0,1-S0]);
 S = x(:,1);
 I = x(:,2);
 R = 1 - S - I;
 
-%% Plot
-% Print ROI
-fig = figure();
+%Plot
+figure();
 plot(t,S,'y'); hold on;
 plot(t,I,'r'); hold on;
 plot(t,R,'g'); hold on;
 
 % Figure paramters
-if R0 < 1
-    title("Time evolution - R_0 < 1");
-else
-    title("Phase Portrait - R_0 > 1");
-end
-
+title("Time evolution - R_0 < 1");
 legend({'Susceptible','Infected','Removed'});
 xlabel('t [day]');
+ylabel('S,I,R [%]');
+ylim([0 1]);
+
+%% R0 > 1
+% Tracjectory
+odefun = @(t,x) SIR(t,x,mu,beta,gamma2);
+[t,x] = ode45(odefun,[0 tf],[S0,1-S0]);
+S = x(:,1);
+I = x(:,2);
+R = 1 - S - I;
+
+%Plot
+figure();
+plot(t,S,'y'); hold on;
+plot(t,I,'r'); hold on;
+plot(t,R,'g'); hold on;
+
+% Figure paramters
+title("Time evolution - R_0 > 1");
+legend({'Susceptible','Infected','Removed'});
+xlabel('t [day]');
+ylabel('S,I,R [%]');
+ylim([0 1]);
+
 
 %% Clean
 rmpath('./utility');
