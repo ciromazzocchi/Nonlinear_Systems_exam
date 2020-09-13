f820ead27cc5f5905478d58f836e6b3611777e1d
ciromazzocchi <ciro.mazzocchi@outlook.com>
2020-09-13 15:34:15 +0200

    Now function is iterative

--- PhasePortait_SIR.m
+++ PhasePortait_SIR.m
@@ -7,72 +7,54 @@ addpath('./models');
 
 %% Paramter of simulation
 beta = 0.4;
-gamma_r0_minor_1 = 0.3;
-gamma_r0_major_1 = 1/14;
 mu = 0.2;
+gamma = [0.3, 1/14];
 tf = 50;
 
 %% Constant of simulation
 [S,I,x0] = getInitialPoint();
 [n_simulation,~] = size(x0);
 
-%% R0 < 1
-gamma = gamma_r0_minor_1;
+%% Start
+for i=1:max(size(gamma))
+    R0 = beta/(gamma(i)+mu);
+    
+    % Print ROI
+    figure();
+    hold on
+    plot([0 1 0 0 0 1], [0 0 0 1 1 0], 'Color', 'g', 'LineWidth',1.0);
 
-% Print ROI
-figure();
-hold on
-plot([0 1 0 0 0 1], [0 0 0 1 1 0], 'Color', 'g', 'LineWidth',1.0);
+    % Plot vector field
+    [xs,xi,Sd,Id] = getMesh(mu,beta,gamma(i));
 
-% Plot vector field
-[xs,xi,Sd,Id] = getMesh(mu,beta,gamma);
+    quiver(xs,xi,Sd,Id);
 
-quiver(xs,xi,Sd,Id);
+    % Plot equilibrium point
+    scatter(1,0,'b','filled');
+    if R0 > 1
+        scatter((gamma(i)+mu)/beta,mu/(gamma(i) + mu) - mu/beta,'b','filled');
+    end
 
-% Plot equilibrium point
-scatter(1,0,'b','filled');
+    % Plot trajectory
+    odefun = @(t,x) SIR(t,x,mu,beta,gamma(i));
+    printTrajectory(x0,odefun);
+    hold off
 
-% Plot trajectory
-odefun = @(t,x) SIR(t,x,mu,beta,gamma);
-printTrajectory(x0,odefun);
-hold off
-
-% Figure paramters
-title("Phase Portrait - R_0 < 1");
-xlabel('S');
-ylabel('I');
-xlim([0 1.1]);
-ylim([-0.1 1]);
-legend({'Region of interest','Vector field','Equilibrum point'});
-
-%% R0 > 1
-gamma = gamma_r0_major_1;
-
-% Print ROI
-figure();
-hold on
-plot([0 1 0 0 0 1], [0 0 0 1 1 0], 'Color', 'g', 'LineWidth',1.0);
-
-% Plot vector field
-[xs,xi,Sd,Id] = getMesh(mu,beta,gamma);
-
-quiver(xs,xi,Sd,Id);
-
-% Plot equilibrium point
-scatter([1; (gamma+mu)/beta],[0; mu/(gamma + mu) - mu/beta],'b','filled');
-
-% Plot trajectory
-odefun = @(t,x) SIR(t,x,mu,beta,gamma);
-printTrajectory(x0,odefun);
-hold off
-
-% Figure paramters
-title("Phase Portrait - R_0 > 1");
-xlabel('S');
-ylabel('I');
-xlim([0 1.1]);
-ylim([-0.1 1]);
-legend({'Region of interest','Vector field','Equilibrum point'});
+    % Figure paramters
+    if R0 < 1
+        title("Phase Portrait - R_0 < 1");
+    elseif R0 > 1
+        title("Phase Portrait - R_0 > 1");
+    else
+        title("Phase Portrait - R_0 = 1");
+    end
+    
+    xlabel('S');
+    ylabel('I');
+    xlim([0 1.1]);
+    ylim([-0.1 1]);
+    legend({'Region of interest','Vector field','Equilibrum point'});
+end
 
 %% Clean
 rmpath('./utility');
