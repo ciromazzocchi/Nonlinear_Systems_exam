324bcafec6110e0bcb496330496c2003281cab48
ciromazzocchi <ciro.mazzocchi@outlook.com>
2020-09-15 14:13:54 +0200

    Deprecated

--- utility/printTrajectory.m
+++
@@ -1,9 +0,0 @@
-function printTrajectory(x0,odefun)
-    [n_simulation,~] = size(x0);
-    tspan = 0:0.1:100;
-    for i=1:n_simulation
-        [t,dxdt] = ode45(odefun,tspan,x0(i,:));
-        plot(dxdt(:,1),dxdt(:,2),'b');
-    end
-end
-
